65abb36f30b612fc56f04b301d3fdfca4cb2d936
전은형 <ooooo5h91@gmail.com>
2021-03-21 12:53:07 +0900

    어댑터 getView 편집중  - 뿌려질 근거 데이터 변수 목록에서 꺼내오기  - 데이터 반영해줄 UI 요소 찾아서 변수로 담기

--- app/src/main/java/com/neppplus/listviewpractice2_20210321/adapters/StudentAdapter.kt
+++ app/src/main/java/com/neppplus/listviewpractice2_20210321/adapters/StudentAdapter.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.neppplus.listviewpractice2_20210321.R
 import com.neppplus.listviewpractice2_20210321.datas.Student
+import org.w3c.dom.Text
 
 class StudentAdapter(
     val mContext : Context,
@@ -33,7 +35,16 @@ class StudentAdapter(
 //        tempRow는 절대 null이 아닐 것이다! => 무조건 결과로 나가도 된다.
         val row = tempRow!!
 
-//        완성된 row가 화면에 뿌려질 결과로 선정
+//        실제 데이터를 꺼내서 > UI 요소들에 반영
+
+//        실제 데이터 변수 => mList 변수 (목록)안에 들어있다 => position에 맞는 데이터 추출
+        val studentData = mList[position]
+
+//        뿌려질 UI 요소를 가져오자 => row 변수 안에 있는 텍스트뷰 등을 꺼내오자
+        val nameTxt = row.findViewById<TextView>(R.id.nameTxt)
+        val ageTxt = row.findViewById<TextView>(R.id.ageTxt)
+
+//        완성된 row가 화면에 뿌려질 결과로선정
         return row
 
 
