6110549456099c82198a369c253a9dda7679726c
전은형 <ooooo5h91@gmail.com>
2021-03-21 11:50:58 +0900

    화면에 뿌려줄 xml을 그려야하는 상황에만 그리도록

--- app/src/main/java/com/neppplus/listviewpractice2_20210321/adapters/StudentAdapter.kt
+++ app/src/main/java/com/neppplus/listviewpractice2_20210321/adapters/StudentAdapter.kt
@@ -2,7 +2,10 @@ package com.neppplus.listviewpractice2_20210321.adapters
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.neppplus.listviewpractice2_20210321.R
 import com.neppplus.listviewpractice2_20210321.datas.Student
 
 class StudentAdapter(
@@ -13,4 +16,20 @@ class StudentAdapter(
 //    xml > 코틀린 변수로 꺼내오도록 도와주는 클래스. (LayoutInflater)
     val inflater = LayoutInflater.from(mContext)
 
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+//        기존의 결과를 버리고 => 새로운 결과를 직접 만들어서 사용하려고 지웠음! 리턴 슈퍼
+//        position : 지금 그려주려는 위치가 어디인지? 알려준다
+//        convertView : 재활용할 요소가 있는지? => 만약에 있다면 활용하고, 없다면 새로 그려주자자
+
+        var tempRow = convertView
+
+//        재활용 할 요소가 없나? 없으면 그려줘야한다
+        if (tempRow == null) {
+//            tempRow 내부가 비어있는 상황 ! => 채워주자
+            tempRow = inflater.inflate(R.layout.student_list_item, null)
+        }
+
+
+    }
 }
