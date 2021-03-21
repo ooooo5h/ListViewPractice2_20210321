8b1563c698472ca9d10f62acf3c35e6422a66515
전은형 <ooooo5h91@gmail.com>
2021-03-21 15:00:07 +0900

    함수 추가 예제

--- app/src/main/java/com/neppplus/listviewpractice2_20210321/datas/Student.kt
+++ app/src/main/java/com/neppplus/listviewpractice2_20210321/datas/Student.kt
@@ -1,7 +1,29 @@
 package com.neppplus.listviewpractice2_20210321.datas
 
+import java.util.*
+
 class Student(
     val name: String,
     val birthYear : Int,
     val address : String ) {
+    
+//    학생 클래스가 수행할 수 있는 기능 - 함수 추가
+
+//    나의 현재 나이를 계산해서 결과로 주는 함수
+
+    fun getKoreanAge() : Int {
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+
+        val koreanAge = currentYear - this.birthYear + 1
+
+        return koreanAge
+    }
+
+    fun addTwoNumbers(num1 : Int, num2 : Int) : Int {
+
+        val result = num1 + num2
+
+        return result
+
+    }
 }
